75661a7393c6141b88f99e65395277ad722486b9
Eduardo Moreno <eduardocmoreno@gmail.com>
2017-07-20 01:22:30 -0300

    version 2.3.0

--- README.md
+++ README.md
@@ -1,4 +1,83 @@
-# edbox
-Modal jQuery Plugin
+# jQuery edbox
+Responsive and lightweight modal plugin.
+Check for examples at the **[plugin page here](https://eduardocmoreno.github.io/jquery.edbox/)**
 
-Demos and Docs: https://eduardocmoreno.github.io/jquery.edbox/
+#### Download
+**bower:** bower install jquery.edbox --save
+**npm:** npm install jquery.edbox --save
+**zip:** [download .zip file](https://eduardocmoreno.github.io/jquery.edbox/jquery.edbox.zip)
+
+#### Options
+Option | Tag attribute | Type | Default | Description
+--- | --- | --- | --- | ---
+target | data-box-target | string | null | DOM element
+html | data-box-html | string | null | html and/or text
+image | data-box-image | string | null | image path
+url | data-box-url | string | null | url path (files, pages, etc...)
+success | data-box-success | string | null | Alert success
+info | data-box-info | string | null | Alert info
+warning | data-box-warning | string | null | Alert warning
+danger | data-box-danger | string | null | Alert danger
+header | data-box-header | string | null | Optional header tag - accepts html and/or text
+footer | data-box-footer | string | null | Optional footer tag - accepts html and/or text
+width | data-box-width | number | null | Set a fixed width value to the modal
+height | data-box-height | number | null | Set a fixed height value to the modal
+addClass | data-box-add-class | string | null | Adds a especific class to the modal
+close | data-box-close | boolean | true | Makes the modal closable or not.
+animation | data-box-animation | boolean | true | Makes the modal animable or not.
+animateOpen | data-box-animate-open | string | 'edbox-animate-open' | CSS animation class on open
+animateClose | data-box-animate-close | string | 'edbox-animate-close' | CSS animation class on close
+beforeOpen | data-box-before-open | function | function() {} | Function callback before open the modal
+afterOpen | data-box-after-open | function | function() {} | Function callback after open the modal
+beforeClose | data-box-before-close | function | function() {} | Function callback before close the modal
+afterClose | data-box-after-close | function | function() {} | Function callback after close the modal
+
+#### Methods
+```javascript
+//set edbox for the set of matched elements
+$('.myModalLink').edbox({ options });
+
+//Init without assigning any element
+$.edbox({ options });
+
+//Make custom settings as defaults
+$.edboxSettings({ options });
+
+//Close event
+$.edbox('close');
+```
+
+#### Change Log
+**v2.3.0**
+* [JS] - Now it's possible to use all options as tag attribute
+
+**v2.2.3**
+* [JS] [CSS] - Some adjustments for better browser compatibility
+
+**v2.2.2**
+* [JS] - CSSImproved close button positioning based on scrollbar width
+* [JS] - Other small improvements
+* [CSS] - New helper class added to the parent box if option close is false
+
+**v2.2.1**
+* [JS] - Improved url option;
+* [JS] - Improved header option;
+* [JS] - Improved validations;
+* [CSS] - Adjustments;
+
+**v2.2.0**
+* [JS] - Fixed image load;
+* [JS] - Fixed callback afterClose;
+* [JS] - New alert options like bootstrap;
+* [CSS] - New CSS properties and adjustments;
+
+**v2.1.0**
+* [JS] [HTML] - New settable attributes "data-box-header" and "data-box-footer";
+* [CSS] - Adjustments;
+
+**v2.0.0**
+* New features;
+* Easy to use;
+* Easy customization;
+* CSS animations;
+* SASS file;
--- dist/jquery.edbox.js
+++ dist/jquery.edbox.js
@@ -1,5 +1,5 @@
 /*
-* jQuery Edbox plugin v.2.2.3
+* jQuery Edbox plugin v.2.3.0
 * @author Eduardo Moreno - eduardocmoreno[at]gmail[dot]com
 * Code under MIT License - http://en.wikipedia.org/wiki/MIT_License
 */
@@ -13,34 +13,42 @@
 
         self.$el = $(el);
 
-        self.attr = {
-            target: self.$el.attr('data-box-target'),
-            html:   self.$el.attr('data-box-html'),
-            image:  self.$el.attr('data-box-image'),
-            url:    self.$el.attr('data-box-url'),
-            header: self.$el.attr('data-box-header'),
-            footer: self.$el.attr('data-box-footer')
-        }
-
         var href = self.$el.attr('href');
         href = (/^(#.*)/).test(href) ? null : href;
 
-        self.target = self.opt.target || self.attr.target;
-        self.html   = self.opt.html   || self.attr.html;
-        self.image  = self.opt.image  || self.attr.image;
-        self.url    = self.opt.url    || self.attr.url || href;
-        self.header = self.opt.header || self.attr.header;
-        self.footer = self.opt.footer || self.attr.footer;
+        self.target       = self.$el.attr('data-box-target')        || self.opt.target;
+        self.html         = self.$el.attr('data-box-html')          || self.opt.html;
+        self.image        = self.$el.attr('data-box-image')         || self.opt.image;
+        self.url          = self.$el.attr('data-box-url')           || self.opt.url || href;
+        self.success      = self.$el.attr('data-box-success')       || self.opt.success;
+        self.info         = self.$el.attr('data-box-info')          || self.opt.info;
+        self.warning      = self.$el.attr('data-box-warning')       || self.opt.warning;
+        self.danger       = self.$el.attr('data-box-danger')        || self.opt.danger;
+        self.header       = self.$el.attr('data-box-header')        || self.opt.header;
+        self.footer       = self.$el.attr('data-box-footer')        || self.opt.footer;
+        self.width        = self.$el.attr('data-box-width')         || self.opt.width;
+        self.height       = self.$el.attr('data-box-height')        || self.opt.height;
+        self.addClass     = self.$el.attr('data-box-add-class')     || self.opt.addClass;
+        self.animateOpen  = self.$el.attr('data-box-animate-open')  || self.opt.animateOpen;
+        self.animateClose = self.$el.attr('data-box-animate-close') || self.opt.animateClose;
+        
+        self.beforeOpen   = self.$el.attr('data-box-before-open')  ? new Function(self.$el.attr('data-box-before-open'))  : self.opt.beforeOpen;
+        self.afterOpen    = self.$el.attr('data-box-after-open')   ? new Function(self.$el.attr('data-box-after-open'))   : self.opt.afterOpen;
+        self.beforeClose  = self.$el.attr('data-box-before-close') ? new Function(self.$el.attr('data-box-before-close')) : self.opt.beforeClose;
+        self.afterClose   = self.$el.attr('data-box-after-close')  ? new Function(self.$el.attr('data-box-after-close'))  : self.opt.afterClose;
 
-        self.$box        = $('<div class="edbox"/>');
-        self.$boxError   = $('<div class="edbox-error"/>');
-        self.$boxLoad    = $('<div class="edbox-load"/>');
-        self.$boxBody    = $('<div class="edbox-body"/>');
-        self.$boxClose   = $('<div class="edbox-close"/>');
-        self.$boxContent = $('<div class="edbox-content"/>');
-        self.$boxHeader  = $('<div class="edbox-header"/>');
-        self.$boxFooter  = $('<div class="edbox-footer"/>');
-        self.$boxTemp    = $('<div class="edbox-temp"/>');
+        self.close        = self.$el.attr('data-box-close')     ? JSON.parse(self.$el.attr('data-box-close'))     : self.opt.close;
+        self.animation    = self.$el.attr('data-box-animation') ? JSON.parse(self.$el.attr('data-box-animation')) : self.opt.animation;
+
+        self.$box         = $('<div class="edbox"/>');
+        self.$boxError    = $('<div class="edbox-error"/>');
+        self.$boxLoad     = $('<div class="edbox-load"/>');
+        self.$boxBody     = $('<div class="edbox-body"/>');
+        self.$boxClose    = $('<div class="edbox-close"/>');
+        self.$boxContent  = $('<section class="edbox-content"/>');
+        self.$boxHeader   = $('<header class="edbox-header"/>');
+        self.$boxFooter   = $('<footer class="edbox-footer"/>');
+        self.$boxTemp     = $('<div class="edbox-temp"/>');
 
         self.animateEvents = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
 
@@ -58,17 +66,17 @@
             self.url;
 
             var alert = 
-            self.opt.success && 'success' ||
-            self.opt.info && 'info' ||
-            self.opt.warning && 'warning' ||
-            self.opt.danger && 'danger';
+            self.success && 'success' ||
+            self.info && 'info' ||
+            self.warning && 'warning' ||
+            self.danger && 'danger';
 
             if(!content && !alert){
                 self.error('Undefined');
                 return;
             }
 
-            self.opt.beforeOpen();
+            self.beforeOpen();
 
             if(self.target){
                 var $target = $(self.target);
@@ -106,15 +114,15 @@
             }
 
             if(alert){
-                self.alert(alert, self.opt[alert]);
+                self.alert(alert, self[alert]);
                 return;
             }
         },
 
         base: function(){
             self.$box
-            .addClass(self.opt.addClass)
-            .addClass(!self.opt.close && 'edbox-close-false')
+            .addClass(self.addClass)
+            .addClass(!self.close && 'edbox-close-false')
             .add(self.$boxClose)
             .on('click', self.events.click);
 
@@ -147,8 +155,8 @@
             self.$box.append(
                 self.$boxBody
                 .css({
-                    width: self.opt.width,
-                    height: self.opt.height
+                    width: self.width,
+                    height: self.height
                 })
                 .append(
                     self.header ? self.$boxHeader.append(self.$boxClose, self.header) : self.$boxClose,
@@ -165,11 +173,12 @@
         },        
 
         toggle: function(toggle, callback){
-            toggle == 'close' && !self.loading && !self.responseError && self.opt.beforeClose();
+            toggle == 'close' && !self.loading && !self.responseError && self.beforeClose();
 
-            if(self.opt.animation){
+            if(self.animation){
                 self.$box.children()
-                .addClass(toggle == 'open' ? self.opt.animateOpen : self.opt.animateClose)
+                .removeClass(self.animateOpen, self.animateClose)
+                .addClass(toggle == 'open' ? self.animateOpen : self.animateClose)
                 .one(self.animateEvents, function() {
                     typeof callback == 'function' ? callback() : self.callback[toggle]();
                 });
@@ -207,10 +216,10 @@
 
         events: {
             click: function(e){
-                self.opt.close && e.target == e.currentTarget && self.toggle('close');
+                self.close && e.target == e.currentTarget && self.toggle('close');
             },
             keydown: function(e){
-                self.opt.close && e.which == 27 && self.toggle('close');
+                self.close && e.which == 27 && self.toggle('close');
             },
             resize: function(){
                 var body_h      = self.$boxBody.outerHeight();
@@ -225,7 +234,7 @@
 
         callback: {
             open: function(){
-                !self.responseError && self.opt.afterOpen();
+                !self.responseError && self.afterOpen();
             },
             close: function(){
                 $(window).off({
@@ -244,7 +253,7 @@
                 
                 $.removeData(window, 'edbox');
                 
-                !self.loading && !self.responseError && self.opt.afterClose();
+                !self.loading && !self.responseError && self.afterClose();
             }
         }
     };
